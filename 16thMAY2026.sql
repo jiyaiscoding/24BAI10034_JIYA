c4d50a43cc45601d593ecf65bc92fdb9fd078107
jiyaiscoding <jiya02arora@gmail.com>
2026-05-18 23:00:01 +0530

    Add files via upload

---
+++ 16thMAY2026.sql
@@ -0,0 +1,43 @@
+show databases;
+ create database VIT;
+ use VIT;
+ create table CSE(
+ sid int8,
+ sname varchar(50),
+ smarks int1
+ );
+  create table Mech(
+ sid int8,
+ sname varchar(50),
+ smarks int1
+ );
+  create table EEE(
+ sid int8,
+ sname varchar(50),
+ smarks int1
+ );
+ 
+ show tables from vitcampus;
+ select * from cse;
+insert into cse values (109, 'Aryan',96); 
+insert into cse values (110, 'Kripa',97); 
+insert into cse values (111, 'Chavi',90); 
+insert into cse values (112, 'Riya',92); 
+insert into cse values (113, 'Kriti',91);
+
+insert into mech values (124, 'Bhumi',89); 
+insert into mech values (125, 'Maya',92); 
+insert into mech values (126, 'Bharvi',84); 
+insert into mech values (127, 'Ravi',86); 
+insert into mech values (128, 'Dipak',85); 
+
+insert into eee values (152, 'Sami',88); 
+insert into eee values (153, 'Dipika',95); 
+insert into eee values (154, 'Rudra',93); 
+insert into eee values (155, 'Samay',90); 
+insert into eee values (156, 'Pranav',92); 
+
+
+     
+
+
